4cf1583ba2465b6847967b2ff9fd52f467e837b6
Sebastian Naranjo <sebnartor@gmail.com>
2022-09-15 20:25:16 -0500

    Rails CRUD completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.destroy_all
+
+prices = (45...75).to_a
+guests = (2...6).to_a
+4.times do
+  Flat.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: prices.sample,
+    number_of_guests: guests.sample
+  )
+end
